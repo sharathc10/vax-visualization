b204cb252e1591bb932446825ed1fa89afabdc3c
Chitra Sharathchandra <vigilant.hum@gmail.com>
2021-01-29 15:21:52 -0500

    Add tetanus and pertussis data and all vaccine data and add reactivity to dropdown for illness selection

--- global.R
+++ global.R
@@ -12,14 +12,46 @@ choice_vaccine <- unique(vax_illness$Vaccine)
 choice_groupings <- c("Region")
 vax_coverage = read.csv("./data/vaccine coverage.csv")
 choice_regions = c("All", unique(vax_coverage$WHO_REGION))
+
 #filter vax coverage data to include population
-vax_2010_2019 = vax_coverage %>% filter(Year>=2010) 
-colnames(vax_2010_2019)[colnames(vax_2010_2019)=="Cname"]="Country"
-#DTP 
-dtp1_by_region=vax_coverage %>% mutate(WHO_REGION=as.factor(WHO_REGION),Year=as.numeric(Year)) %>%  
-  filter(Year>=2010 & Vaccine=="DTP1") %>% 
+pop=read.csv("./data/pop.csv")
+colnames(vax_coverage)[colnames(vax_coverage)=="Cname"]="Country"
+total_population_by_year = pop %>% group_by(Year) %>% summarise(total_pop=sum(pop,na.rm=T))
+
+#Vaccine breakdown 
+vax_coverage_final = inner_join(vax_coverage,pop) %>% inner_join(.,total_population_by_year) %>% 
+  mutate(final_coverage=(Percent_covrage*pop)/total_pop)
+
+dtp_by_region=vax_coverage_final %>% 
+  mutate(WHO_REGION=as.factor(WHO_REGION),Year=as.numeric(Year)) %>% 
+  filter(Vaccine=="DTP1") %>% 
   group_by(WHO_REGION,Year) %>% 
-  summarise(avg=mean(Percent_covrage,na.rm=T)) 
+  summarise(avg=mean(final_coverage,na.rm=T)) 
+
+ipv_by_region=vax_coverage_final %>% 
+  mutate(WHO_REGION=as.factor(WHO_REGION),Year=as.numeric(Year)) %>% 
+  filter(Vaccine=="IPV1") %>% 
+  group_by(WHO_REGION,Year) %>% 
+  summarise(avg=mean(final_coverage,na.rm=T)) 
+
+mcv_by_region=vax_coverage_final %>% 
+  mutate(WHO_REGION=as.factor(WHO_REGION),Year=as.numeric(Year)) %>% 
+  filter(Vaccine=="MCV1") %>% 
+  group_by(WHO_REGION,Year) %>% 
+  summarise(avg=mean(final_coverage,na.rm=T)) 
+
+rcv_by_region=vax_coverage_final %>% 
+  mutate(WHO_REGION=as.factor(WHO_REGION),Year=as.numeric(Year)) %>% 
+  filter(Vaccine=="RCV1") %>% 
+  group_by(WHO_REGION,Year) %>% 
+  summarise(avg=mean(final_coverage,na.rm=T)) 
+
+japenc_by_region=vax_coverage_final %>% 
+  mutate(WHO_REGION=as.factor(WHO_REGION),Year=as.numeric(Year)) %>% 
+  filter(Vaccine=="JapEnc") %>% 
+  group_by(WHO_REGION,Year) %>% 
+  summarise(avg=mean(final_coverage,na.rm=T)) 
+
 #diphtheria data
 diphtheria = read.csv("./data/diphtheria.csv")
 colnames(diphtheria)[colnames(diphtheria)=="Cname"]="Country"
@@ -28,5 +60,32 @@ diphtheria_by_region_2010_2019 = flatten_illness_data(diphtheria) %>%
   filter(Year>=2010) %>% 
   group_by(WHO_REGION,Year) %>% 
   summarise(total_cases=sum(reported_cases,na.rm=T)) 
-all_diphtheria_dtp1=inner_join(diphtheria_by_region_2010_2019 ,dtp1_by_region)
+
+all_diphtheria_dtp1=inner_join(diphtheria_by_region_2010_2019 ,dtp_by_region)
+
+#tetanus data
+tetanus1 = read.csv("./data/tetanus.csv")
+tetanus2 = read.csv("./data/ttetanus.csv")
+tetanus = rbind(tetanus1,tetanus2)
+colnames(tetanus)[colnames(tetanus)=="Cname"]="Country"
+tetanus_by_region_2010_2019 = flatten_illness_data(tetanus) %>% 
+  mutate(WHO_REGION=as.factor(WHO_REGION),Year=as.numeric(Year)) %>%  
+  filter(Year>=2010) %>% 
+  group_by(WHO_REGION,Year) %>% 
+  summarise(total_cases=sum(reported_cases,na.rm=T)) 
+
+all_tetanus_dtp1=inner_join(tetanus_by_region_2010_2019 ,dtp_by_region)
+
+#pertussis data
+pertussis = read.csv("./data/pertussis.csv")
+colnames(pertussis)[colnames(pertussis)=="Cname"]="Country"
+pertussis_by_region_2010_2019 = flatten_illness_data(pertussis) %>% 
+  mutate(WHO_REGION=as.factor(WHO_REGION),Year=as.numeric(Year)) %>%  
+  filter(Year>=2010) %>% 
+  group_by(WHO_REGION,Year) %>% 
+  summarise(total_cases=sum(reported_cases,na.rm=T)) 
+
+all_pertussis_dtp1=inner_join(pertussis_by_region_2010_2019 ,dtp_by_region)
+
+
 
--- server.R
+++ server.R
@@ -5,14 +5,42 @@ library(ggplot2)
 
 shinyServer(function(input, output){
     
+    data <- reactive({
+       if (input$selectedIllness == "Diphtheria") {
+        dataSet <- all_diphtheria_dtp1
+       } else if (input$selectedIllness == "Tetanus") {
+           dataSet <- all_tetanus_dtp1
+       } else if (input$selectedIllness == "Pertussis") {
+           dataSet <- all_pertussis_dtp1
+       }
+           
+        dataSet
+    })
+    
+    scale <- reactive({
+        if (input$selectedIllness == "Diphtheria") {
+            scaleVal <- 10000
+        } else if (input$selectedIllness == "Tetanus") {
+            scaleVal <- 8000
+        } else if (input$selectedIllness == "Pertussis") {
+            scaleVal <- 40000
+        }
+        
+        scaleVal
+    })
+    
     # show illness graph
     output$illness <- renderPlot({
-        ggplot(all_diphtheria_dtp1, aes(Year, total_cases)) + 
-            geom_bar(data=all_diphtheria_dtp1,stat="identity") + 
-            geom_line(data=all_diphtheria_dtp1, aes(y=avg*100)) + 
+        df=data()
+        scaleVal=scale()
+        
+        ggplot(df, aes(Year, total_cases)) + 
+            geom_bar(data=df,stat="identity") + 
+            geom_line(data=df, aes(y=avg*scaleVal),color="red") + 
             scale_y_continuous(sec.axis= sec_axis(~./100, name="avg")) +
             facet_wrap(~ WHO_REGION)
-            })
+            
+        })
    
     # show data using DataTable
     output$table <- DT::renderDataTable({
